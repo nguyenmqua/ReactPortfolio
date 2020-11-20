6b469a05a8413380f9ed43cb142a78faeafbc495
Qua Nguyen <Nguyenmqua@gmail.com>
2020-11-19 18:14:29 -0700

    build folder

---
+++ build/precache-manifest.27e4182c4cecf81b1eb3ab17855733d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c8be6958d5c0baa1d7fc91a3667c5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2128aba9366361584387",
+    "url": "/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "4de0ad42912bfcee5cbf",
+    "url": "/static/css/main.d0e5a6f8.chunk.css"
+  },
+  {
+    "revision": "2128aba9366361584387",
+    "url": "/static/js/2.e887e4b1.chunk.js"
+  },
+  {
+    "revision": "e011f5e9df9f5ae9640732e671c27460",
+    "url": "/static/js/2.e887e4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de0ad42912bfcee5cbf",
+    "url": "/static/js/main.7b539b08.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
