28c0a83189cbf408bbc17d6a11474c4a51fc3642
Qua Nguyen <Nguyenmqua@gmail.com>
2021-02-12 20:14:08 -0700

    rewrote package.json

---
+++ build/precache-manifest.09aee37b47d83df1b69617b88d8b9832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc946c68f9fc7fc775658207ea2c911",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "4ab59d4122f096c32ebf",
+    "url": "/ReactPortfolio/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "0fae03589d0480593fcd",
+    "url": "/ReactPortfolio/static/css/main.91b0c375.chunk.css"
+  },
+  {
+    "revision": "4ab59d4122f096c32ebf",
+    "url": "/ReactPortfolio/static/js/2.217b713e.chunk.js"
+  },
+  {
+    "revision": "d4e12a94c796fd7b75b6ff2547d06713",
+    "url": "/ReactPortfolio/static/js/2.217b713e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fae03589d0480593fcd",
+    "url": "/ReactPortfolio/static/js/main.04ce83cc.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da6a243e75ce22093877e8d2c4c8b3fe.js"
+  "/ReactPortfolio/precache-manifest.09aee37b47d83df1b69617b88d8b9832.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
