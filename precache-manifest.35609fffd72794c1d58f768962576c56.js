df31cd26ba2c93888c71127a97ff184ffe979b08
Qua Nguyen <Nguyenmqua@gmail.com>
2021-03-26 11:37:53 -0600

    Updates

--- precache-manifest.35609fffd72794c1d58f768962576c56.js
+++ precache-manifest.35609fffd72794c1d58f768962576c56.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5c6ed877972b703125bb43527a5d40b",
+    "revision": "83077b813098815a78335f74a22f262e",
     "url": "/ReactPortfolio/index.html"
   },
   {
-    "revision": "7fd0cd010bcb88b06632",
+    "revision": "2672cd2f5d5f37508962",
     "url": "/ReactPortfolio/static/css/2.22ca87da.chunk.css"
   },
   {
-    "revision": "3a77918e1ba9e5263e99",
+    "revision": "214ebaf603a8b0c11e8d",
     "url": "/ReactPortfolio/static/css/main.91dd11d8.chunk.css"
   },
   {
-    "revision": "7fd0cd010bcb88b06632",
-    "url": "/ReactPortfolio/static/js/2.6d118ff1.chunk.js"
+    "revision": "2672cd2f5d5f37508962",
+    "url": "/ReactPortfolio/static/js/2.1ea05d8e.chunk.js"
   },
   {
     "revision": "d4e12a94c796fd7b75b6ff2547d06713",
-    "url": "/ReactPortfolio/static/js/2.6d118ff1.chunk.js.LICENSE.txt"
+    "url": "/ReactPortfolio/static/js/2.1ea05d8e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a77918e1ba9e5263e99",
-    "url": "/ReactPortfolio/static/js/main.7ee7e5d3.chunk.js"
+    "revision": "214ebaf603a8b0c11e8d",
+    "url": "/ReactPortfolio/static/js/main.67c98739.chunk.js"
   },
   {
     "revision": "97a1c7d0b592075495b7",
